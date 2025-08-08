8e277e53d4d2528bed933f7bc4db24d5666d0ed8
Cristian De Gracia <cristian.degracia@INV0000319.workstation.bizneo.local>
2025-08-08 15:19:48 +0200

    Create app and login layout

--- lib/comet_web/components/layouts.ex
+++ lib/comet_web/components/layouts.ex
@@ -27,41 +27,32 @@ defmodule CometWeb.Layouts do
   """
   attr :flash, :map, required: true, doc: "the map of flash messages"
 
-  attr :current_scope, :map,
-    default: nil,
-    doc: "the current [scope](https://hexdocs.pm/phoenix/scopes.html)"
+  attr :current_scope, :map, default: nil
 
   slot :inner_block, required: true
 
   def app(assigns) do
     ~H"""
-    <header class="navbar px-4 sm:px-6 lg:px-8">
-      <div class="flex-1">
-        <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
-        </a>
-      </div>
-      <div class="flex-none">
-        <ul class="flex flex-column px-1 space-x-4 items-center">
-          <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
-          </li>
-          <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
-          </li>
-          <li>
-            <.theme_toggle />
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </header>
+    <.sidebar current_scope={@current_scope}>
+      <main class="px-4 py-20 sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl space-y-4">
+          {render_slot(@inner_block)}
+        </div>
+      </main>
+    </.sidebar>
 
+    <.flash_group flash={@flash} />
+    """
+  end
+
+  attr :flash, :map, required: true, doc: "the map of flash messages"
+
+  attr :current_scope, :map, default: nil
+
+  slot :inner_block, required: true
+
+  def login(assigns) do
+    ~H"""
     <main class="px-4 py-20 sm:px-6 lg:px-8">
       <div class="mx-auto max-w-2xl space-y-4">
         {render_slot(@inner_block)}
@@ -148,6 +139,80 @@ defmodule CometWeb.Layouts do
       >
         <.icon name="hero-moon-micro" class="size-4 opacity-75 hover:opacity-100" />
       </button>
+    </div>
+    """
+  end
+
+  attr :current_scope, :map, default: nil
+
+  slot :inner_block, required: true
+
+  def sidebar(assigns) do
+    ~H"""
+    <div class="sidebar drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content flex flex-col">
+        <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">
+          Open drawer
+        </label>
+
+        <nav class="navbar justify-between border-b border-base-300">
+          <div class="navbar-start flex">
+            <ul class="menu menu-horizontal px-1">
+              <li><a>Settings</a></li>
+            </ul>
+          </div>
+          <div class="navbar-end">
+            <.theme_toggle />
+          </div>
+        </nav>
+
+        {render_slot(@inner_block)}
+      </div>
+      <div class="drawer-side !overflow-visible">
+        <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
+
+        <div class="menu bg-base-200 text-base-content min-h-full w-70 p-2">
+          <div class="navigation">
+            <a href="/" class="flex-1 flex w-fit items-center gap-2 p-2">
+              <img src={~p"/images/logo.svg"} width="44" />
+              <span class="text-2xl font-semibold">Comet</span>
+            </a>
+
+            <ul class="menu bg-base-200 text-base-content min-h-full w-full">
+              <li><.link href={~p"/"}><.icon name="hero-home" />Home</.link></li>
+              <li><.link href={~p"/"}><.icon name="hero-magnifying-glass" />Browser</.link></li>
+              <li><.link href={~p"/"}><.icon name="hero-rectangle-stack" />Backlog</.link></li>
+            </ul>
+          </div>
+
+          <.dropdown
+            :if={@current_scope}
+            class="w-full"
+            position="dropdown-right dropdown-end"
+          >
+            <:trigger>
+              <div class="user-settings hover:bg-base-100 p-2 rounded">
+                <div class="flex gap-2">
+                  <.avatar size="small" />
+                  <div class="info">
+                    <span class="font-medium">Joseph</span>
+                    <span class="text-xs font-light text-base-content/50">
+                      {@current_scope.user.email}
+                    </span>
+                  </div>
+                </div>
+                <.icon name="hero-cog-6-tooth" />
+              </div>
+            </:trigger>
+
+            <:actions>
+              <.link href={~p"/users/settings"}>Settings</.link>
+              <.link href={~p"/users/log-out"} method="delete">Log out</.link>
+            </:actions>
+          </.dropdown>
+        </div>
+      </div>
     </div>
     """
   end
