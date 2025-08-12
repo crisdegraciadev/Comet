b2c5b82a97fb008d1d71804a41b06eb90db64ea7
jj-tena <josejustotena0@gmail.com>
2025-08-12 14:35:40 +0200

    Fix incorrect layout selection

--- lib/comet_web/live/user_live/registration.ex
+++ lib/comet_web/live/user_live/registration.ex
@@ -7,7 +7,7 @@ defmodule CometWeb.UserLive.Registration do
   @impl true
   def render(assigns) do
     ~H"""
-    <Layouts.app flash={@flash} current_scope={@current_scope}>
+    <Layouts.login flash={@flash} current_scope={@current_scope}>
       <div class="mx-auto max-w-sm">
         <div class="text-center">
           <.header>
@@ -37,7 +37,7 @@ defmodule CometWeb.UserLive.Registration do
           </.button>
         </.form>
       </div>
-    </Layouts.app>
+    </Layouts.login>
     """
   end
 
