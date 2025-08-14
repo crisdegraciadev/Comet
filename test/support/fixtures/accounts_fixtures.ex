4c8971ecfe159b88badac72d592a6293f5914515
jj-tena <josejustotena0@gmail.com>
2025-08-14 10:34:03 +0200

    Remove email delivering from account fixtures

--- test/support/fixtures/accounts_fixtures.ex
+++ test/support/fixtures/accounts_fixtures.ex
@@ -29,16 +29,7 @@ defmodule Comet.AccountsFixtures do
 
   def user_fixture(attrs \\ %{}) do
     user = unconfirmed_user_fixture(attrs)
-
-    token =
-      extract_user_token(fn url ->
-        Accounts.deliver_login_instructions(user, url)
-      end)
-
-    {:ok, {user, _expired_tokens}} =
-      Accounts.login_user_by_magic_link(token)
-
-    user
+    set_password(user)
   end
 
   def user_scope_fixture do
@@ -51,39 +42,9 @@ defmodule Comet.AccountsFixtures do
   end
 
   def set_password(user) do
-    {:ok, {user, _expired_tokens}} =
+    {:ok, {user, _}} =
       Accounts.update_user_password(user, %{password: valid_user_password()})
 
     user
-  end
-
-  def extract_user_token(fun) do
-    {:ok, captured_email} = fun.(&"[TOKEN]#{&1}[TOKEN]")
-    [_, token | _] = String.split(captured_email.text_body, "[TOKEN]")
-    token
-  end
-
-  def override_token_authenticated_at(token, authenticated_at) when is_binary(token) do
-    Comet.Repo.update_all(
-      from(t in Accounts.UserToken,
-        where: t.token == ^token
-      ),
-      set: [authenticated_at: authenticated_at]
-    )
-  end
-
-  def generate_user_magic_link_token(user) do
-    {encoded_token, user_token} = Accounts.UserToken.build_email_token(user, "login")
-    Comet.Repo.insert!(user_token)
-    {encoded_token, user_token.token}
-  end
-
-  def offset_user_token(token, amount_to_add, unit) do
-    dt = DateTime.add(DateTime.utc_now(:second), amount_to_add, unit)
-
-    Comet.Repo.update_all(
-      from(ut in Accounts.UserToken, where: ut.token == ^token),
-      set: [inserted_at: dt, authenticated_at: dt]
-    )
   end
 end
