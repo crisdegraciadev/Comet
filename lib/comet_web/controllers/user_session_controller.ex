f894f2d154686ddf900b82f105ca1e1d9147e985
jj-tena <josejustotena0@gmail.com>
2025-08-14 10:36:01 +0200

    Remove email delivering from user token and session controller

--- lib/comet_web/controllers/user_session_controller.ex
+++ lib/comet_web/controllers/user_session_controller.ex
@@ -4,41 +4,16 @@ defmodule CometWeb.UserSessionController do
   alias Comet.Accounts
   alias CometWeb.UserAuth
 
-  def create(conn, %{"_action" => "confirmed"} = params) do
-    create(conn, params, "User confirmed successfully.")
-  end
-
-  def create(conn, params) do
-    create(conn, params, "Welcome back!")
-  end
-
-  # magic link login
-  defp create(conn, %{"user" => %{"token" => token} = user_params}, info) do
-    case Accounts.login_user_by_magic_link(token) do
-      {:ok, {user, tokens_to_disconnect}} ->
-        UserAuth.disconnect_sessions(tokens_to_disconnect)
-
-        conn
-        |> put_flash(:info, info)
-        |> UserAuth.log_in_user(user, user_params)
-
-      _ ->
-        conn
-        |> put_flash(:error, "The link is invalid or it has expired.")
-        |> redirect(to: ~p"/users/log-in")
-    end
-  end
-
-  # email + password login
-  defp create(conn, %{"user" => user_params}, info) do
+  # Login with email and password
+  def create(conn, %{"user" => user_params}) do
     %{"email" => email, "password" => password} = user_params
 
     if user = Accounts.get_user_by_email_and_password(email, password) do
       conn
-      |> put_flash(:info, info)
+      |> put_flash(:info, "Welcome back!")
       |> UserAuth.log_in_user(user, user_params)
     else
-      # In order to prevent user enumeration attacks, don't disclose whether the email is registered.
+      # To prevent user enumeration attacks, do not indicate whether the email is registered
       conn
       |> put_flash(:error, "Invalid email or password")
       |> put_flash(:email, String.slice(email, 0, 160))
@@ -46,19 +21,21 @@ defmodule CometWeb.UserSessionController do
     end
   end
 
+  # Password update
   def update_password(conn, %{"user" => user_params} = params) do
     user = conn.assigns.current_scope.user
     true = Accounts.sudo_mode?(user)
     {:ok, {_user, expired_tokens}} = Accounts.update_user_password(user, user_params)
 
-    # disconnect all existing LiveViews with old sessions
+    # Disconnect all old active sessions
     UserAuth.disconnect_sessions(expired_tokens)
 
     conn
     |> put_session(:user_return_to, ~p"/settings/account")
-    |> create(params, "Password updated successfully!")
+    |> create(params)
   end
 
+  # Logout
   def delete(conn, _params) do
     conn
     |> put_flash(:info, "Logged out successfully.")
