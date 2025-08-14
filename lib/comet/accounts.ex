4f5ca71bacd667432fa8d28f21d9c6492a18ef6d
jj-tena <josejustotena0@gmail.com>
2025-08-14 10:34:29 +0200

    Remove email delivering from account context

--- lib/comet/accounts.ex
+++ lib/comet/accounts.ex
@@ -5,8 +5,7 @@ defmodule Comet.Accounts do
 
   import Ecto.Query, warn: false
   alias Comet.Repo
-
-  alias Comet.Accounts.{User, UserToken, UserNotifier}
+  alias Comet.Accounts.{User, UserToken}
 
   ## Database getters
 
@@ -136,27 +135,6 @@ defmodule Comet.Accounts do
   end
 
   @doc """
-  Updates the user email using the given token.
-
-  If the token matches, the user email is updated and the token is deleted.
-  """
-  def update_user_email(user, token) do
-    context = "change:#{user.email}"
-
-    Repo.transact(fn ->
-      with {:ok, query} <- UserToken.verify_change_email_token_query(token, context),
-           %UserToken{sent_to: email} <- Repo.one(query),
-           {:ok, user} <- Repo.update(User.email_changeset(user, %{email: email})),
-           {_count, _result} <-
-             Repo.delete_all(from(UserToken, where: [user_id: ^user.id, context: ^context])) do
-        {:ok, user}
-      else
-        _ -> {:error, :transaction_aborted}
-      end
-    end)
-  end
-
-  @doc """
   Returns an `%Ecto.Changeset{}` for changing the user password.
 
   See `Comet.Accounts.User.password_changeset/3` for a list of supported options.
@@ -213,91 +191,6 @@ defmodule Comet.Accounts do
   end
 
   @doc """
-  Gets the user with the given magic link token.
-  """
-  def get_user_by_magic_link_token(token) do
-    with {:ok, query} <- UserToken.verify_magic_link_token_query(token),
-         {user, _token} <- Repo.one(query) do
-      user
-    else
-      _ -> nil
-    end
-  end
-
-  @doc """
-  Logs the user in by magic link.
-
-  There are three cases to consider:
-
-  1. The user has already confirmed their email. They are logged in
-     and the magic link is expired.
-
-  2. The user has not confirmed their email and no password is set.
-     In this case, the user gets confirmed, logged in, and all tokens -
-     including session ones - are expired. In theory, no other tokens
-     exist but we delete all of them for best security practices.
-
-  3. The user has not confirmed their email but a password is set.
-     This cannot happen in the default implementation but may be the
-     source of security pitfalls. See the "Mixing magic link and password registration" section of
-     `mix help phx.gen.auth`.
-  """
-  def login_user_by_magic_link(token) do
-    {:ok, query} = UserToken.verify_magic_link_token_query(token)
-
-    case Repo.one(query) do
-      # Prevent session fixation attacks by disallowing magic links for unconfirmed users with password
-      {%User{confirmed_at: nil, hashed_password: hash}, _token} when not is_nil(hash) ->
-        raise """
-        magic link log in is not allowed for unconfirmed users with a password set!
-
-        This cannot happen with the default implementation, which indicates that you
-        might have adapted the code to a different use case. Please make sure to read the
-        "Mixing magic link and password registration" section of `mix help phx.gen.auth`.
-        """
-
-      {%User{confirmed_at: nil} = user, _token} ->
-        user
-        |> User.confirm_changeset()
-        |> update_user_and_delete_all_tokens()
-
-      {user, token} ->
-        Repo.delete!(token)
-        {:ok, {user, []}}
-
-      nil ->
-        {:error, :not_found}
-    end
-  end
-
-  @doc ~S"""
-  Delivers the update email instructions to the given user.
-
-  ## Examples
-
-      iex> deliver_user_update_email_instructions(user, current_email, &url(~p"/users/settings/confirm-email/#{&1}"))
-      {:ok, %{to: ..., body: ...}}
-
-  """
-  def deliver_user_update_email_instructions(%User{} = user, current_email, update_email_url_fun)
-      when is_function(update_email_url_fun, 1) do
-    {encoded_token, user_token} = UserToken.build_email_token(user, "change:#{current_email}")
-
-    Repo.insert!(user_token)
-    UserNotifier.deliver_update_email_instructions(user, update_email_url_fun.(encoded_token))
-  end
-
-  @doc """
-  Delivers the magic link login instructions to the given user.
-  """
-  def deliver_login_instructions(%User{} = user, magic_link_url_fun)
-      when is_function(magic_link_url_fun, 1) do
-    {encoded_token, user_token} = UserToken.build_email_token(user, "login")
-    Repo.insert!(user_token)
-    UserNotifier.deliver_login_instructions(user, magic_link_url_fun.(encoded_token))
-  end
-
-  @doc """
   Deletes the signed token with the given context.
   """
   def delete_user_session_token(token) do
@@ -311,14 +204,13 @@ defmodule Comet.Accounts do
     Repo.transact(fn ->
       with {:ok, user} <- Repo.update(changeset) do
         tokens_to_expire = Repo.all_by(UserToken, user_id: user.id)
-
         Repo.delete_all(from(t in UserToken, where: t.id in ^Enum.map(tokens_to_expire, & &1.id)))
-
         {:ok, {user, tokens_to_expire}}
       end
     end)
   end
 
+  ## Changesets
 
   @doc """
   Returns an `%Ecto.Changeset{}` for changing the user registration.
@@ -344,6 +236,23 @@ defmodule Comet.Accounts do
   def update_profile(profile, attrs) do
     profile
     |> Comet.Accounts.Profile.changeset(attrs, %{user: profile.user}, require_fields: true)
+    |> Repo.update()
+  end
+
+  @doc """
+  Updates a user's email directly without requiring confirmation.
+
+  ## Examples
+
+      iex> update_user_email_without_confirmation(user, "new@example.com")
+      {:ok, %User{}}
+
+      iex> update_user_email_without_confirmation(user, "invalid")
+      {:error, %Ecto.Changeset{}}
+  """
+  def update_user_email_without_confirmation(%User{} = user, new_email) when is_binary(new_email) do
+    user
+    |> User.email_changeset(%{"email" => new_email})
     |> Repo.update()
   end
 end
