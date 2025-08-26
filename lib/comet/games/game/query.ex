59a04546d357ccb174854a70ff687b90639af278
jj-tena <josejustotena0@gmail.com>
2025-08-26 16:34:40 +0200

    Add methods to get games only from current user

--- lib/comet/games/game/query.ex
+++ lib/comet/games/game/query.ex
@@ -1,13 +1,14 @@
 defmodule Comet.Games.Game.Query do
   import Ecto.Query
-
   alias Comet.Repo
   alias Comet.Games.Game
+  alias Comet.Accounts.User
 
-  def all(), do: Game |> Repo.all()
+  def all(), do: Repo.all(Game)
 
-  def all(filter) do
+  def all(%User{id: user_id}, filter \\ %{}) do
     Game
+    |> where([g], g.user_id == ^user_id)
     |> with_status(filter["status"])
     |> with_platform(filter["platform"])
     |> search_by(filter["name"])
@@ -15,28 +16,27 @@ defmodule Comet.Games.Game.Query do
   end
 
   defp with_status(query, status) when status in ~w(completed in_progress pending) do
-    where(query, status: ^status)
+    where(query, [g], g.status == ^String.to_existing_atom(status))
   end
-
   defp with_status(query, _), do: query
 
   defp search_by(query, value) when value in ["", nil], do: query
-
   defp search_by(query, value) do
     where(query, [g], ilike(g.name, ^"%#{value}%"))
   end
 
   defp with_platform(query, platform) when platform in ~w(pc ps1 ps2 ps3 ps4 ps5 psp switch) do
-    where(query, platform: ^platform)
+    where(query, [g], g.platform == ^String.to_existing_atom(platform))
   end
-
   defp with_platform(query, _), do: query
 
-  def get!(id) when is_integer(id) do
-    Game |> Repo.get!(id)
+  def get!(%User{id: user_id}, id) when is_integer(id) do
+    Game
+    |> where([g], g.user_id == ^user_id)
+    |> Repo.get!(id)
   end
 
-  def get!(id) when is_binary(id) do
-    id |> String.to_integer() |> get!()
+  def get!(%User{} = user, id) when is_binary(id) do
+    id |> String.to_integer() |> get!(user)
   end
 end
