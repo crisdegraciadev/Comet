b0d1c50560267cd7862ba7fa4a10311ba9d2c100
jj-tena <josejustotena0@gmail.com>
2025-09-01 11:57:45 +0200

    Replace svg for hero icon

--- lib/comet_web/live/browser_live/collection.ex
+++ lib/comet_web/live/browser_live/collection.ex
@@ -73,7 +73,7 @@ defmodule CometWeb.BrowserLive.Collection do
       </.form>
 
       <div :if={@api_key == nil or @api_key == ""} class="alert alert-warning">
-        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-2.5L13.732 4c-.77-.833-1.964-.833-2.732 0L3.732 16.5c-.77.833.192 2.5 1.732 2.5z" /></svg>
+        <.icon name="hero-exclamation-triangle" class="w-6 h-6 text-white" />
         <span>You need to configure your SteamGridDB API key in
         <.link href={~p"/settings/api_key"} class="link link-primary">Settings</.link>
         to search for games.</span>
